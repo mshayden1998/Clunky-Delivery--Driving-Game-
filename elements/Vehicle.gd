e4772ed1c71f38e801b2089be771dad806904517
Marcelo Silveira Hayden <mshayden.1998@gmail.com>
2022-12-26 10:52:00 -0300

    vehicle upgrade
    jump and rotation mechanics

--- elements/Vehicle.gd
+++ elements/Vehicle.gd
@@ -2,17 +2,17 @@ extends RigidBody2D
 
 onready var b_wheel = $BackWheel
 onready var jump_error_detectors = [
-	$BackWheel/BWJumpError,
-	$FrontWheel/FWJumpError
+	$BWJumpError,
+	$FWJumpError
 ]
 var engine_force = 1500.0
 var max_speed = 5000.0
 var jump_force = -5000
+var rotation_force = 800.0
 
 
 func _ready():
-	b_wheel.set_friction(.5)
-	$FrontWheel.set_friction(.5)
+	pass
 
 
 func _physics_process(delta):
@@ -25,7 +25,13 @@ func get_input(delta):
 		b_wheel.apply_torque_impulse(engine_force * delta * 60)
 	if Input.is_action_pressed("player_down"):
 		b_wheel.apply_torque_impulse(-engine_force * delta * 60)
+	# Chassis rotation mechanic
+	if Input.is_action_pressed("player_left"):
+		apply_torque_impulse(-rotation_force * delta * 60)
+	if Input.is_action_pressed("player_right"):
+		apply_torque_impulse(rotation_force * delta * 60)
 	# Jump mechanic
-	
-	if Input.is_action_just_pressed("player_jump"):
-		apply_central_impulse(Vector2(0, jump_force))
+	for i in jump_error_detectors.size():
+		if jump_error_detectors[i].is_colliding():
+			if Input.is_action_just_pressed("player_jump"):
+				apply_central_impulse(Vector2(0, jump_force))
