e27d72537783a0d5d4127bb98f89fb507f50defd
Marcelo Silveira Hayden <mshayden.1998@gmail.com>
2022-12-27 23:12:32 -0300

    first level almost made

--- elements/Vehicle.gd
+++ elements/Vehicle.gd
@@ -1,14 +1,10 @@
 extends RigidBody2D
 
-onready var b_wheel = $BackWheel
-onready var jump_error_detectors = [
-	$BWJumpError,
-	$FWJumpError
-]
-var engine_force = 1500.0
-var max_speed = 5000.0
-var jump_force = -5000
-var rotation_force = 800.0
+onready var b_wheel := $BackWheel
+var engine_force := 1500.0
+var max_speed := 5000.0
+var rotation_force := 900.0
+const fps := 60
 
 
 func _ready():
@@ -22,16 +18,11 @@ func _physics_process(delta):
 func get_input(delta):
 	# Forward and backward movements
 	if Input.is_action_pressed("player_up"):
-		b_wheel.apply_torque_impulse(engine_force * delta * 60)
+		b_wheel.apply_torque_impulse(engine_force * delta * fps)
 	if Input.is_action_pressed("player_down"):
-		b_wheel.apply_torque_impulse(-engine_force * delta * 60)
+		b_wheel.apply_torque_impulse(-engine_force * delta * fps)
 	# Chassis rotation mechanic
 	if Input.is_action_pressed("player_left"):
-		apply_torque_impulse(-rotation_force * delta * 60)
+		apply_torque_impulse(-rotation_force * delta * fps)
 	if Input.is_action_pressed("player_right"):
-		apply_torque_impulse(rotation_force * delta * 60)
-	# Jump mechanic
-	for i in jump_error_detectors.size():
-		if jump_error_detectors[i].is_colliding():
-			if Input.is_action_just_pressed("player_jump"):
-				apply_central_impulse(Vector2(0, jump_force))
+		apply_torque_impulse(rotation_force * delta * fps)
